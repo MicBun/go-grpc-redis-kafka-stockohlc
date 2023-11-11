a2869c883d20cd04091b7566926a71e36c4f2a7a
Michael Buntarman <michaelboentarman@gmail.com>
2023-11-11 13:06:56 +0700

    [SOHLC-0011] Rework Kafka Topic, combine it into one topic] (#1)

--- server/message/main.go
+++ server/message/main.go
@@ -2,6 +2,7 @@ package message
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/MicBun/go-grpc-redis-kafka-stockohlc-server/pubsub"
 	"github.com/MicBun/go-grpc-redis-kafka-stockohlc-server/stock"
@@ -20,17 +21,21 @@ func NewMessage(
 	}
 }
 
-func (h *Message) FileCreated(ctx context.Context, msg *message.Message) error {
-	defer msg.Ack()
-	return h.stockManager.UpdateStockOnFileCreate(ctx, string(msg.Payload))
-}
-
 func (h *Message) FileUpdated(ctx context.Context, msg *message.Message) error {
 	defer msg.Ack()
-	return h.stockManager.UpdateStockOnFileUpdate(ctx, string(msg.Payload))
+
+	body := new(FileUpdatedSchemaMessage)
+	if err := json.Unmarshal(msg.Payload, body); err != nil {
+		return err
+	}
+
+	if body.IsCreate {
+		return h.stockManager.UpdateStockOnFileCreate(ctx, body.FileName)
+	} else {
+		return h.stockManager.UpdateStockOnFileUpdate(ctx, body.FileName)
+	}
 }
 
 func (h *Message) Load(subscriber pubsub.Subscriber) {
-	subscriber.Subscribe(pubsub.TopicFileCreated, h.FileCreated)
 	subscriber.Subscribe(pubsub.TopicFileUpdated, h.FileUpdated)
 }
